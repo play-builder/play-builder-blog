d7dae1b7283bb8d4760750c04c4f8c1e37de13a8
play-builder <playbuilder47@gmail.com>
2026-06-12 13:48:49 +0900

    Add: deploy blog on Cloudflare

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -1,45 +1,60 @@
+
 import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
-    ogImage: "default-og.jpg",
-    lang: "en",
-    timezone: "Asia/Bangkok",
+    url: "https://play-builder.pages.dev/",
+    title: "Play Builder",
+    description:
+      "DevOps, Kubernetes, AWS, Terraform, Web3, and platform engineering notes by Kai. Sharing real-world experiences in cloud infrastructure, blockchain operations, and production systems.",
+    author: "PlayBuilder",
+    profile: "https://github.com/play-builder",
+    ogImage: "play-builder-image.jpg",
+    lang: "ko",
+    timezone: "Asia/Seoul",
     dir: "ltr",
   },
+
   posts: {
     perPage: 4,
     perIndex: 4,
     scheduledPostMargin: 15 * 60 * 1000,
   },
+
   features: {
     lightAndDarkMode: true,
     dynamicOgImage: true,
     showArchives: true,
     showBackButton: true,
     editPost: {
-      enabled: true,
-      url: "https://github.com/satnaing/astro-paper/edit/main/",
+      enabled: false,
     },
     search: "pagefind",
   },
+
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    {
+      name: "youtube",
+      url: "https://www.youtube.com/@play-builder47",
+    },
+        {
+      name: "github",
+      url: "https://github.com/play-builder",
+    },
+    {
+      name: "mail",
+      url: "mailto:playbuilder47@gmail.com",
+    },
   ],
+
   shareLinks: [
-    { name: "whatsapp", url: "https://wa.me/?text=" },
-    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
-    { name: "x",        url: "https://x.com/intent/post?url=" },
-    { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
-    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+    {
+      name: "x",
+      url: "https://x.com/intent/post?url=",
+    },
+    {
+      name: "mail",
+      url: "mailto:?subject=Play%20Builder&body=",
+    },
   ],
 });
